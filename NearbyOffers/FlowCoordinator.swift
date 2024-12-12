0f48035530bbf02f270ca49f8ea4ebc6a2ec0c50
Marcos Patrick <marcospatrick039474@gmail.com>
2024-12-11 21:44:16 -0300

    adding a new file to manage the screens view

---
+++ NearbyOffers/FlowCoordinator.swift
@@ -0,0 +1,34 @@
+//
+//  FlowCoordinator.swift
+//  NearbyOffers
+//
+//  Created by patrickson on 10/12/24.
+//
+
+import Foundation
+import UIKit // necessario para criar interface
+
+
+// este Coordinator ira controlar a pilha de telas
+class FlowCoordinator{
+    private var navigationController: UINavigationController?
+    
+    // class initializer
+    public init(){  }
+    
+    // start the first Controller
+    func start() -> UINavigationController? {
+        
+        // criando tela
+        let startViewController = UIViewController()
+        startViewController.view.backgroundColor = .red
+        
+        // navegacao recebe a tela
+        self.navigationController = UINavigationController(
+            rootViewController: startViewController
+        );
+        
+        return self.navigationController
+    }
+    
+}
