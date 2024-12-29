f3d46bff925a5a2bfb5c2405a72a2ba6ca5f700e
Marcos Patrick <marcospatrick039474@gmail.com>
2024-12-29 11:10:09 -0800

    resetting

---
+++ NearbyOffers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearbyOffers
+//
+//  Created by ptkzin on 29/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
